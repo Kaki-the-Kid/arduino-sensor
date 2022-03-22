3bf0a020d7743bc83ec66b8d24205fc3bd5f227e
Karsten Reitan Sørensen <krs@aarhustech.dk>
2022-03-22 14:23:16 +0100

    Init code

--- sketch_ky-001-dsp18b20-temp_sensor/sketch_ky-001-dsp18b20-temp_sensor.ino
+++ sketch_ky-001-dsp18b20-temp_sensor/sketch_ky-001-dsp18b20-temp_sensor.ino
@@ -1,9 +1,73 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Opsætning af knap
+int buttonpin13 = 13;
+int buttonState13 = 0;
+
+#define del 1000
+#include <LiquidCrystal.h>
+
+/*
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ */
+ 
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int offsetTime = 0;
+
+
+
+void setup(void)
+{
+  // Init display
+  lcd.cursor();
+  lcd.blink();
+  //lcd.begin(16, 2);          // set up the LCD's number of columns and rows:
+  //lcd.print("Temperatur"); // Print a message to the LCD.
+  
+  
+  //Serial.begin(9600);// start serial port
+  //Serial.println("Dallas Temperature IC Control Library Demo");
+  
+  // Start up the library
+  //sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  //Serial.print("Requesting temperatures...");
+  //sensors.requestTemperatures(); // Send the command to get temperatures
+  //Serial.println("DONE");
+  //Serial.print("Temperature for Device 1 is: ");
+  //Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
 
+  //lcd.setCursor(0, 1); // set the cursor to column 0, line 1
+  //lcd.print( sensors.getTempCByIndex(0) );
 }
