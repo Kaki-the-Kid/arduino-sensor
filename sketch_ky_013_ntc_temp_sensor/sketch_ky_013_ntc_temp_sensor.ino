52f117d0d28e5f89a61cabcddb5fa0caef75b22e
Karsten Reitan Sørensen <krs@aarhustech.dk>
2022-03-23 09:14:35 +0100

    Init commit

--- sketch_ky_013_ntc_temp_sensor/sketch_ky_013_ntc_temp_sensor.ino
+++ sketch_ky_013_ntc_temp_sensor/sketch_ky_013_ntc_temp_sensor.ino
@@ -1,19 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-
-int ThermistorPin = A0;
-int Vo;
+int   ThermistorPin = A0;
+int   Vo;
 float R1 = 10000; // value of R1 on board
 float logR2, R2, T;
 float c1 = 0.001129148, c2 = 0.000234125, c3 = 0.0000000876741; //steinhart-hart coeficients for thermistor
+
 
 void setup() {
   Serial.begin(9600);
@@ -21,13 +11,18 @@ void setup() {
 
 void loop() {
   Vo = analogRead(ThermistorPin);
+  delay(50);
   R2 = R1 * (1023.0 / (float)Vo - 1.0); //calculate resistance on thermistor
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); // temperature in Kelvin
   T = T - 273.15; //convert Kelvin to Celcius
  // T = (T * 9.0)/ 5.0 + 32.0; //convert Celcius to Farenheit
+ 
   Serial.print("Temperature: "); 
   Serial.print(T);
-  Serial.println(" C"); 
-  delay(500);
+  Serial.println(" C");
+  Serial.print("Vo ");
+  Serial.println(Vo);
+  
+  delay(3000);
 }
