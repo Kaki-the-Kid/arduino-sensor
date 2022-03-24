38c9fa0092cfaf5baf1cb31c349c1f2f13cca584
Karsten Reitan Sørensen <krs@aarhustech.dk>
2022-03-24 08:02:19 +0100

    Init commit

--- sketch_ky_006_piezo_sound/sketch_ky_006_piezo_sound_A-HA.ino
+++ sketch_ky_006_piezo_sound/sketch_ky_006_piezo_sound_A-HA.ino
@@ -1,24 +1,54 @@
+//A-ha!
+//by GeneralSpud
+
+// For this to work, we need the pitches library
+
+#include "pitches.h"
+
 int buzzer = 9; // set the buzzer control digital IO pin
 
+// Two things need to be created: the array for the notes of the melody (in order)
+// and the duration of each (think of it like sheet music in two parts)
+
+// BOTH ARRAYS MUST BE THE SAME SIZE!
+
+// The melody array 
+int melody[] = {
+  NOTE_FS5, NOTE_FS5, NOTE_D5, NOTE_B4, NOTE_B4, NOTE_E5, 
+  NOTE_E5, NOTE_E5, NOTE_GS5, NOTE_GS5, NOTE_A5, NOTE_B5, 
+  NOTE_A5, NOTE_A5, NOTE_A5, NOTE_E5, NOTE_D5, NOTE_FS5, 
+  NOTE_FS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5
+};
+
+// The note duration, 8 = 8th note, 4 = quarter note, etc.
+int durations[] = {
+  8, 8, 8, 4, 4, 4, 
+  4, 5, 8, 8, 8, 8, 
+  8, 8, 8, 4, 4, 4, 
+  4, 5, 8, 8, 8, 8
+};
+// determine the length of the arrays to use in the loop iteration
+int songLength = sizeof(melody)/sizeof(melody[0]);
+
 void setup() {
-  pinMode(buzzer, OUTPUT); // set pin 8 as output
+ //We don't need anything here
 }
 
 void loop() {
-  for (int i = 0; i < 100; i++) {  // make a sound
-    digitalWrite(buzzer, HIGH); // send high signal to buzzer 
-    delay(1); // delay 1ms
-    digitalWrite(buzzer, LOW); // send low signal to buzzer
-    delay(1);
+  // Iterate through both arrays
+  // Notice how the iteration variable thisNote is created in the parenthesis
+  // The for loop stops when it is equal to the size of the melody array
+  for (int thisNote = 0; thisNote < songLength; thisNote++)
+  {
+    // determine the duration of the notes that the computer understands
+    // divide 1000 by the value, so the first note lasts for 1000/8 milliseconds
+    int duration = 1000/ durations[thisNote];
+    
+    tone(buzzer, melody[thisNote], duration);
+    // pause between notes
+    int pause = duration * 1.3;
+    delay(pause);
+    // stop the tone
+    noTone(buzzer);
   }
-
-delay(50);
-  for (int j = 0; j < 100; j++) { //make another sound
-    digitalWrite(buzzer, HIGH);
-    delay(2); // delay 2ms
-    digitalWrite(buzzer, LOW);
-    delay(2);
-  }
-  
-  delay(500);
 }
