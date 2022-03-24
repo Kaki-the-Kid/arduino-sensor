08be80a1f8f9a430ad512dce8006266cc9c11ff7
Karsten Reitan Sørensen <krs@aarhustech.dk>
2022-03-24 12:05:45 +0100

    Init commit

--- sketch_hc-sr04_ultrasonic_sensor/sketch_hc-sr04_ultrasonic_sensor.ino
+++ sketch_hc-sr04_ultrasonic_sensor/sketch_hc-sr04_ultrasonic_sensor.ino
@@ -1,9 +1,47 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+*/
+
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+
+const int rs = 12, en = 11, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int trigPin = 3;
+const int echoPin = 2;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
-  // put your setup code here, to run once:
-  int ww = 0;
+  lcd.begin(16, 2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * 0.034 / 2;
+  distanceInch = duration * 0.0133 / 2;
+  
+  lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Distance: "); // Prints string "Distance" on the LCD
+  lcd.print(distanceCm); // Prints the distance value from the sensor
+  lcd.print(" cm");
+  delay(10);
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Distance: ");
+  lcd.print(distanceInch);
+  lcd.print(" inch");
+  delay(10);
 }
